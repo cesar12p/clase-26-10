1f7fe62e6eb37df86b30aaa0f3c31eec76b21a3a
cesar12p <15460531@itcolima.edu.mx>
2019-10-26 17:06:16 -0500

    Ejercicio 2

---
+++ Eje2.py
@@ -0,0 +1,18 @@
+def main():
+    a=int(input("Ingrese su salario"))
+    b=int(input("Ingrese su categoria ej: 1 2 3 4"))
+    if(b==1):
+        desc=a*.15
+        sueldo=a+desc
+    elif(b==2):
+        desc=a*.10
+        sueldo=a+desc
+    elif(b==3):
+        desc=a*.8
+        sueldo=a+desc
+    elif(b==4):
+        desc=a*.7
+        sueldo=a+desc
+    print("Aumento: "+str(desc)+" Sueldo Total: "+str(sueldo))
+if __name__ == "__main__":
+    main()
